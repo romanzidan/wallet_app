3e9adae5e06d90feb806efef1cffdb6b56161a2f
rzkzx <romanzidanramadhan@gmail.com>
2023-12-13 02:17:49 +0800

    Created BottomAppBar Home

--- lib/ui/pages/home.dart
+++ lib/ui/pages/home.dart
@@ -10,6 +10,85 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      bottomNavigationBar: Theme(
+        data: ThemeData(
+          splashColor: Colors.transparent,
+          highlightColor: Colors.transparent,
+        ),
+        child: BottomAppBar(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 10,
+          ),
+          color: whiteColor,
+          shape: const CircularNotchedRectangle(),
+          clipBehavior: Clip.antiAlias,
+          notchMargin: 6,
+          elevation: 0,
+          child: BottomNavigationBar(
+            type: BottomNavigationBarType.fixed,
+            elevation: 0,
+            backgroundColor: whiteColor,
+            selectedItemColor: blueColor,
+            unselectedItemColor: blackColor,
+            showSelectedLabels: true,
+            showUnselectedLabels: true,
+            selectedLabelStyle: blueTextStyle.copyWith(
+              fontSize: 10,
+              fontWeight: medium,
+            ),
+            unselectedLabelStyle: blackTextStyle.copyWith(
+              fontSize: 10,
+              fontWeight: medium,
+            ),
+            items: [
+              BottomNavigationBarItem(
+                icon: Image.asset(
+                  'assets/ic_overview.png',
+                  width: 20,
+                  color: blueColor,
+                ),
+                label: 'Overview',
+              ),
+              BottomNavigationBarItem(
+                icon: Image.asset(
+                  'assets/ic_history.png',
+                  width: 20,
+                ),
+                label: 'History',
+              ),
+              const BottomNavigationBarItem(
+                icon: SizedBox(
+                  width: 10,
+                ),
+                label: '',
+              ),
+              BottomNavigationBarItem(
+                icon: Image.asset(
+                  'assets/ic_statistic.png',
+                  width: 20,
+                ),
+                label: 'Statistic',
+              ),
+              BottomNavigationBarItem(
+                icon: Image.asset(
+                  'assets/ic_reward.png',
+                  width: 20,
+                ),
+                label: 'Reward',
+              ),
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: purpleColor,
+        child: Image.asset(
+          'assets/ic_plus_circle.png',
+          width: 24,
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       backgroundColor: lightBackgroundColor,
       body: ListView(
         padding: const EdgeInsets.symmetric(
